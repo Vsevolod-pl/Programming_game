99a7aec86805a11b7921f22ee218e582c47ed84b
Vsevolod-pl <vsevolod-pl@yandex.ru>
2019-12-23 22:03:47 +0300

    Fixed simple test

--- simple_test.py
+++ simple_test.py
@@ -1,10 +1,10 @@
-from Python_client.run_commands import run_match
+from run_all import run_all
 import unittest
 
 
 class TestBots(unittest.TestCase):
-    def test_run():
-        result_match = run_match(["bot1", "bot2"], ["Python_client.bot1", "Python_client.bot2"], [1, 2])
+    def test_run(self):
+        result_match = run_all(["bot1", "bot2"], ["Python_client.bot1", "Python_client.bot2"], [1, 2])
         self.assertEqual(result_match, {'bot1': 'win', 'bot2': 'lose'})
 
 unittest.main()
